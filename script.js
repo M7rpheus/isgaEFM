13fb753a7f550572d3fa328a5af2d2746c0a7ae4
ADAM SEHNANI <adamsehnani@iisga.com>
2026-06-02 15:32:06 +0100

    revectoir

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const badge = document.querySelector('.badge');
+    badge.textContent = "Version 1.0.0";
+    
+    console.log("Examen chargé avec succès.");
+});
